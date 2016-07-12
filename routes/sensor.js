df1fa62ec2bc7136fb393c79488464eb80fa8df6
You Kobayashi <sweep.3092@gmail.com>
2016-07-12 08:57:26 +0000

    get data  from sensor

--- routes/sensor.js
+++ routes/sensor.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var sensorLib = require('node-dht-sensor');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json();
+  var sensor = {
+    initialize: function () {
+      return sensorLib.initialize(11, 4);
+    },
+    read: function () {
+      var readout = sensorLib.read();
+      return {
+        temperature: readout.temperature.toFixed(2),
+        humidity: readout.humidity.toFixed(2)
+      }
+    }
+  };
+
+  if (sensor.initialize()) {
+    res.json(sensor.read());
+  } else {
+    console.warn('Failed to initialize sensor');
+  }
 });
 
 module.exports = router;
