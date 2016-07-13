f776664c80c5440b56c6961804218c2f9e110c46
You Kobayashi <sweep.3092@gmail.com>
2016-07-13 03:31:51 +0000

    できた

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -14,12 +14,16 @@ $(document).ready(function () {
     },
     axis: {
       y: {
+        min: 10,
+        max: 40,
         label: {
           text: 'Temperature',
           position: 'outer-middle',
         },
       },
       y2: {
+        min: 20,
+        max: 100,
         label: {
           text: 'Humidity',
           position: 'outer-middle',
@@ -41,7 +45,7 @@ $(document).ready(function () {
     }).done(function (response_data) {
       chart_data[0].push(parseFloat(response_data.temperature));
       chart_data[1].push(parseFloat(response_data.humidity));
-      if (chart_data[0].length > 11) {
+      if (chart_data[0].length > 31) {
         chart_data[0].splice(1, 1);
         chart_data[1].splice(1, 1);
       }
