ec8739d3a5066062d60bbfc7ea47bf20c584bf70
You Kobayashi <sweep.3092@gmail.com>
2016-07-26 19:48:17 +0900

    センサーの初期化を一度だけに変更

--- routes/sensor.js
+++ routes/sensor.js
@@ -2,26 +2,23 @@ var express = require('express');
 var router = express.Router();
 var sensorLib = require('node-dht-sensor');
 
+var sensor = {
+  initialize: function () {
+    return sensorLib.initialize(11, 4);
+  },
+  read: function () {
+    var readout = sensorLib.read();
+    return {
+      temperature: readout.temperature.toFixed(2),
+      humidity: readout.humidity.toFixed(2)
+    }
+  }
+};
+sensor.initialize();
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  var sensor = {
-    initialize: function () {
-      return sensorLib.initialize(11, 4);
-    },
-    read: function () {
-      var readout = sensorLib.read();
-      return {
-        temperature: readout.temperature.toFixed(2),
-        humidity: readout.humidity.toFixed(2)
-      }
-    }
-  };
-
-  if (sensor.initialize()) {
-    res.json(sensor.read());
-  } else {
-    console.warn('Failed to initialize sensor');
-  }
+  res.json(sensor.read());
 });
 
 module.exports = router;
